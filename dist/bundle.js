e4d39f23c2298fd2795fc6143de1c6fd8d604058
PB-vt <cv.juanp@gmail.com>
2022-04-05 10:29:43 -0600

    Added webpack config

---
+++ dist/bundle.js
@@ -0,0 +1,10 @@
+var MyLib;
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+
+MyLib = __webpack_exports__;
+/******/ })()
+;
